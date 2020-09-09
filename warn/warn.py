ad397a31ae2a4ece5a71b0410a088c1664a994d8
Anoncyb <69906082+Anoncyb@users.noreply.github.com>
2020-09-09 17:46:42 +0700

    Update warn.py

--- warn/warn.py
+++ warn/warn.py
@@ -15,3 +15,10 @@ im='\033[1;97m'
 tot='\x1b[105m\x1b[37;1m'
 
 sop='\x1b[0m'
+m = '\033[1;31m' # merah
+b = '\033[1;36m' # biru
+k = '\033[1;33m' # kuning
+h = '\033[1;32m' # hijau
+u = '\033[1;35m' # ungu
+p = '\033[1;37m' # putih
+ut = '\033[1;34m' # ungu tua
