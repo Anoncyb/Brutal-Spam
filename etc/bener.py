1bf04f2fef39be5fed20b421d3dd2d36d2f40e50
Anoncyb <69906082+Anoncyb@users.noreply.github.com>
2020-09-09 17:42:43 +0700

    Add files via upload

---
+++ etc/bener.py
@@ -0,0 +1,19 @@
+from warn.warn import *
+# -*- coding: utf-8 -*-
+import os,sys,time
+
+def wr(z):
+	for x in z + '\n':
+		sys.stdout.write(x);sys.stdout.flush();time.sleep(1./1000)
+def bnr():
+ wr(f"""{b}╔═╗┌┐┌┌─┐┌┐┌┌─┐┬ ┬┌┐   {b}╔╗ ┬─┐┬ ┬┌┬┐┌─┐┬  
+{p}╠═╣││││ │││││  └┬┘├┴┐  ╠╩╗├┬┘│ │ │ ├─┤│   
+╩ ╩┘└┘└─┘┘└┘└─┘ ┴ └─┘  ╚═╝┴└─└─┘ ┴ ┴ ┴┴─┘ 
+{k}════════════════════════════════════════════
+{p}[{h}+{p}]{h}Team   {p}: {b}Anoncyb
+{p}[{h}+{p}]{h}Github {p}: {b}https://github.com/Anoncyb
+{p}[{h}+{p}]{h}Script {p}: {b}Brutal SMS
+{k}════════════════════════════════════════════
+""")
+
+
